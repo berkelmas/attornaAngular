37d62c1755d8ff7e8cab54e9d37e93f57b975835
unknown <berkelmas96@gmail.com>
2019-06-26 04:27:51 +0300

    navbar completed with animation adn started sidenavbar open-close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef, Renderer2, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('menuChanged', [
+      transition('void => entered', [style({transform: 'translateY(-100px)', opacity: 0}), animate('500ms')])
+    ])
+  ]
 })
-export class AppComponent {
-  title = 'attornaAngular';
+export class AppComponent implements OnInit {
+  displaySmallNav: boolean = false;
+
+  @HostListener('window:scroll', ['$event']) handleScroll(e) {
+    if (window.pageYOffset > 50) {
+
+    }
+    if (window.pageYOffset > 250) {
+      this.displaySmallNav = true;
+    } else {
+      this.displaySmallNav = false;
+    }
+    if (window.pageYOffset < 50) {
+
+    }
+  }
+
+  ngOnInit() {
+
+  }
+
+  constructor(private renderer: Renderer2) {};
 }
