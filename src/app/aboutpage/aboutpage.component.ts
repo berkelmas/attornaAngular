b7746c6a1f989d63b1d59c2a113c970b59c47d46
berk elmas <berkelmas@berks-MacBook.home>
2019-06-27 02:36:56 +0300

    FAQ done without animations

--- src/app/aboutpage/aboutpage.component.ts
+++ src/app/aboutpage/aboutpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-aboutpage',
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutpage.component.scss']
 })
 export class AboutpageComponent implements OnInit {
+  firstQuestion: boolean = false;
+  secondQuestion: boolean = false;
+  thirdQuestion: boolean = false;
+  fourthQuestion: boolean = false;
+  fifthQuestion: boolean = false;
+
+  @ViewChild('firstAnswer', {static: false}) firstAnswer: ElementRef;
+  @ViewChild('secondAnswer', {static: false}) secondAnswer: ElementRef;
+  @ViewChild('thirdAnswer', {static: false}) thirdAnswer: ElementRef;
+  @ViewChild('fourthAnswer', {static: false}) fourthAnswer: ElementRef;
+  @ViewChild('fifthAnswer', {static: false}) fifthAnswer: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openQuestion(question: number) {
+    console.log(this.secondQuestion);
+
+    switch(true) {
+      case (question === 1):
+        this.firstQuestion = !this.firstQuestion;
+        break;
+      case (question === 2):
+        this.secondQuestion = !this.secondQuestion;
+        break;
+      case (question === 3):
+        this.thirdQuestion = !this.thirdQuestion;
+        break;
+      case (question === 4):
+        this.fourthQuestion = !this.fourthQuestion;
+        break;
+      case (question === 5):
+        this.fifthQuestion = !this.fifthQuestion;
+        break;
+      default:
+        break;
+    }
   }
 
 }
