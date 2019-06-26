03fd2e4333dbe1ecabe36a6929c012732064cdcb
unknown <berkelmas96@gmail.com>
2019-06-26 15:45:16 +0300

    sidebar completed with animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,23 +14,25 @@ import {
   animations: [
     trigger('menuChanged', [
       transition('void => entered', [style({transform: 'translateY(-100px)', opacity: 0}), animate('500ms')])
+    ]),
+    trigger('siderbarChanged', [
+      transition('void => sidebarEntered', [style({transform: 'translateX(350px)', opacity: 0}), animate('500ms')]),
+      transition('sidebarEntered => void', [animate('500ms', style({transform: 'translateX(350px)'}))]),
+
+      transition('void => backEntered', [style({opacity : 0}), animate('500ms')]),
+      transition('mainPage => mainPageClosed', [style({transform: 'translateX(-350px)'}), animate('500ms', style({transform: 'translateX(0)'}))])
     ])
   ]
 })
 export class AppComponent implements OnInit {
   displaySmallNav: boolean = false;
+  displaySidebar: boolean = false;
 
   @HostListener('window:scroll', ['$event']) handleScroll(e) {
-    if (window.pageYOffset > 50) {
-
-    }
     if (window.pageYOffset > 250) {
       this.displaySmallNav = true;
     } else {
       this.displaySmallNav = false;
-    }
-    if (window.pageYOffset < 50) {
-
     }
   }
 
@@ -39,4 +41,9 @@ export class AppComponent implements OnInit {
   }
 
   constructor(private renderer: Renderer2) {};
+
+  openMenu() {
+    this.displaySidebar = !this.displaySidebar;
+  }
+
 }
